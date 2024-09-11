7162631357839327441f302c3a40b0e75580b20b
MonsieurCiao <luchaokas@gmail.com>
2024-09-11 12:57:56 +0200

    added calculator

--- Python/11-09-24.py
+++ Python/11-09-24.py
@@ -1,8 +1,21 @@
 # calculator
-a = input('first number: ')
-b = input('second number: ')
+a = float(input('first number: '))
+b = float(input('second number: '))
 operation = input('operation: ')
 
+def switch(operation):
+  if operation == '+':
+    return a + b
+  elif operation == '-':
+    return a - b
+  elif operation == '*':
+    return a * b
+  elif operation == '/':
+    if b != 0:
+      return a/b
+    else:
+      return "Can't divide by zero"
+  else:
+    return 'Did you type in a valid operation'
 
-
-
+print('Your result = ', switch(operation))
